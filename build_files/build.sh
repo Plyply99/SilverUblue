b50c752bc9e3a79f292f9ed461c6c11a725fc444
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2025-11-29 16:43:31 -0500

    Update build.sh
    Openrazer drama, remove.

--- build_files/build.sh
+++ build_files/build.sh
@@ -21,6 +21,6 @@ dnf5 -y install bat bat-extras emacs eza fastfetch ghostty input-remapper mangoh
 dnf5 -y install cliphist hyprland hyprpicker niri swww mako waybar waypaper blueman kde-connect kf6-kitemmodels network-manager-applet fuzzel nwg-drawer nwg-look mate-polkit
 
 # openrazer install for silverblue
-curl -o - https://download.opensuse.org/repositories/hardware:/razer/Fedora_$(rpm -E %fedora)/hardware:razer.repo | sudo tee /etc/yum.repos.d/razer.repo
-dnf5 -y install openrazer-meta
+#curl -o - https://download.opensuse.org/repositories/hardware:/razer/Fedora_$(rpm -E %fedora)/hardware:razer.repo | sudo tee /etc/yum.repos.d/razer.repo
+#dnf5 -y install openrazer-meta
 
