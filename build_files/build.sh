8c19eeee467b2a284912e91d7ddc4f5a9d06d69f
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2025-11-29 18:43:10 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -18,7 +18,7 @@ dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedo
 dnf5 -y install bat bat-extras emacs eza fastfetch ghostty input-remapper mangohud rpm-build steam vkBasalt #  openrazer-meta
 
 # Window manager stuff
-dnf5 -y install cliphist hyprland hyprpicker niri swww mako waybar waypaper blueman kde-connect kf6-kitemmodels network-manager-applet fuzzel nwg-drawer nwg-look mate-polkit
+dnf5 -y install cliphist hyprland hyprpicker niri swww mako waybar waypaper blueman kde-connect kf6-kitemmodels network-manager-applet fuzzel nwg-drawer nwg-look mate-polkit mpvpaper
 
 # openrazer install for silverblue
 #curl -o - https://download.opensuse.org/repositories/hardware:/razer/Fedora_$(rpm -E %fedora)/hardware:razer.repo | sudo tee /etc/yum.repos.d/razer.repo
