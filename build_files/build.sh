8229afcf949af04e8d621c2e4cd6597177a02a65
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2025-11-29 13:06:40 -0500

    Update build.sh
    Move into /build_files

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
-set -ouex pipefail
+### Build file for SilverUblue
 
-### Install packages
+# Enable copr repos
+dnf -y copr enable cyqsimon/bat-extras 
+dnf -y copr enable scottames/ghostty
+dnf -y copr enable sdegler/hyprland 
+dnf -y cope enable terjeros/eza 
+dnf -y copr enable avengemedia/dms-git 
+dnf -y copr enable enable yalter/niri-git
+dnf -y copr enable tofik/nwg-shell
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# Install extra packages
+dnf install -y bat bat-extras emacs eza fastfetch ghostty input-remapper mangohud openrazer-daemon rpm-build steam vkBasalt
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Window manager stuff
+dnf install -y cliphist hyprland hyprpicker niri swww mako waybar waypaper blueman kdeconnect kf6-kitemmodels network-manager-applet fuzzel nwg-drawer nwg-look mate-polkit
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# Akmod signing stuff for secure boot and openrazer
