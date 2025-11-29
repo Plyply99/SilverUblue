e7d02cc1933164cabd7125f5e5c5be7e8687cadc
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2025-11-29 16:20:43 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -25,9 +25,19 @@ curl -o - https://download.opensuse.org/repositories/hardware:/razer/Fedora_$(rp
 dnf5 -y install openrazer-meta
 
 # Add akmod signing
-git clone https://github.com/CheariX/silverblue-akmods-keys
-cd silverblue-akmods-keys
-bash setup.sh
+git clone https://github.com/CheariX/silverblue-akmods-keys && cd "$(basename "$_" .git)
+mkdir -p /root/rpmbuild/SOURCES/
+cp \
+  macros.kmodtool \
+  /etc/pki/akmods/private/private_key.priv \
+  /etc/pki/akmods/certs/public_key.der \
+  /root/rpmbuild/SOURCES
+rpmbuild -ba akmods-keys.spec
+mv /root/rpmbuild/RPMS/noarch/akmods-keys-0.0.2-8.fc$(rpm -E %fedora).noarch.rpm .
+rm -f $(find /root/rpmbuild -type f | grep -E 'akmods-keys|public_key.der|private_key.priv|macros.kmodtool')
+find /root/rpmbuild -type d | sort -r | while read d; do
+  rm -df "$d" 2>/dev/null
+done
 dnf5 -y install akmods-keys-0.0.2-8.fc$(rpm -E %fedora).noarch.rpm
 
 groupadd plugdev
