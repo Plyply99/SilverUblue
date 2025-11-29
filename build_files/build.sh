9b8e74a06f2ff604c3d5f4a2e7063123bf274c86
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2025-11-29 16:07:26 -0500

    Update build.sh - openrazer test

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,15 +14,24 @@ dnf5 -y copr enable tofik/nwg-shell
 # Steam repo
 dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo
 
-# openrazer install for silverblue
-#  curl -o - https://download.opensuse.org/repositories/hardware:/razer/Fedora_$(rpm -E %fedora)/hardware:razer.repo | sudo tee /etc/yum.repos.d/razer.repo
-#  groupadd plugdev
-#  usermod -aG plugdev $USER
-
 # Install extra packages
 dnf5 -y install bat bat-extras emacs eza fastfetch ghostty input-remapper mangohud rpm-build steam vkBasalt #  openrazer-meta
 
 # Window manager stuff
 dnf5 -y install cliphist hyprland hyprpicker niri swww mako waybar waypaper blueman kde-connect kf6-kitemmodels network-manager-applet fuzzel nwg-drawer nwg-look mate-polkit
+
+# openrazer install for silverblue
+curl -o - https://download.opensuse.org/repositories/hardware:/razer/Fedora_$(rpm -E %fedora)/hardware:razer.repo | sudo tee /etc/yum.repos.d/razer.repo
+dnf5 -y install openrazer-meta
+
+# Add akmod signing
+git clone https://github.com/CheariX/silverblue-akmods-keys
+cd silverblue-akmods-keys
+bash setup.sh
+dnf5 -y install akmods-keys-0.0.2-8.fc$(rpm -E %fedora).noarch.rpm
+
+groupadd plugdev
+#usermod -aG plugdev $USER
+
 
 # Akmod signing stuff for secure boot and openrazer
