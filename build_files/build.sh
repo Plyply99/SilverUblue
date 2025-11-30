e2f3ce3f0b3d07b4aa89753dbc42b63522734f1e
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2025-11-30 16:10:36 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,10 +2,6 @@
 
 ### Build file for SilverUblue
 
-# Custom os-release
-RUN echo VARIANT="SilverUblue" && echo VARIANT_ID=https://github.com/Plyply99/SilverUblue
-
-
 # Enable copr repos
 dnf5 -y copr enable cyqsimon/bat-extras 
 dnf5 -y copr enable scottames/ghostty
@@ -23,6 +19,10 @@ dnf5 -y install bat bat-extras emacs eza fastfetch ghostty input-remapper mangoh
 
 # Window manager stuff
 dnf5 -y install cliphist hyprland hyprpicker niri swww mako waybar waypaper blueman kde-connect kf6-kitemmodels network-manager-applet fuzzel nwg-drawer nwg-look mate-polkit mpvpaper
+
+# Custom os-release
+RUN echo PRETTY_NAME="SilverUblue 43"
+
 
 # openrazer install for silverblue
 #curl -o - https://download.opensuse.org/repositories/hardware:/razer/Fedora_$(rpm -E %fedora)/hardware:razer.repo | sudo tee /etc/yum.repos.d/razer.repo
