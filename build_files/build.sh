57346fc3915b0c4332aa3be4bc3f8655c57d20a4
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2025-12-01 05:01:59 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,7 +20,7 @@ dnf5 -y install bat bat-extras emacs eza fastfetch ghostty input-remapper mangoh
 # Window manager stuff
 dnf5 -y install cliphist hyprland hyprpicker niri swww mako waybar waypaper blueman kde-connect kf6-kitemmodels network-manager-applet fuzzel nwg-drawer nwg-look mate-polkit mpvpaper
 
-
+Run echo VARIANT="SilverUblue 43" && echo VARIANT_ID=SilverUblue by Plyply
 
 # openrazer install for silverblue
 #curl -o - https://download.opensuse.org/repositories/hardware:/razer/Fedora_$(rpm -E %fedora)/hardware:razer.repo | sudo tee /etc/yum.repos.d/razer.repo
