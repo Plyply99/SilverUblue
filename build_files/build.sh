84b66d8534f4a41474118b3e9c26809c0af0dc7e
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2025-12-04 13:06:25 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,18 +11,15 @@ dnf5 -y copr enable avengemedia/dms-git
 dnf5 -y copr enable yalter/niri-git
 dnf5 -y copr enable tofik/nwg-shell
 
-# Steam repo
+# Steam repo Silverblue
 #dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo
 
-# Install extra packages
-dnf5 -y install bat bat-extras emacs eza fastfetch ghostty input-remapper mangohud rpm-build steam vkBasalt #  openrazer-meta
+# Install extra packages Silverblue
+#dnf5 -y install bat bat-extras emacs eza fastfetch ghostty input-remapper mangohud rpm-build steam vkBasalt #  openrazer-meta
+
+# Install extra packages Bazzite
+dnf5 -y install bat bat-extras emacs eza ghostty gnome-software gnome-software-rpm-ostree
 
 # Window manager stuff
 dnf5 -y install cliphist hyprland hyprpicker niri swww mako waybar waypaper blueman kde-connect kf6-kitemmodels network-manager-applet fuzzel nwg-drawer nwg-look mate-polkit mpvpaper
 
-# Run echo VARIANT="SilverUblue 43" && echo VARIANT_ID=SilverUblue by Plyply
-
-# openrazer install for silverblue
-#curl -o - https://download.opensuse.org/repositories/hardware:/razer/Fedora_$(rpm -E %fedora)/hardware:razer.repo | sudo tee /etc/yum.repos.d/razer.repo
-#dnf5 -y install openrazer-meta
-#
