581c152294fe841f869fe49c3d87d537c72e7cf0
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2025-12-05 14:21:26 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,15 +12,16 @@ dnf5 -y copr enable yalter/niri-git
 dnf5 -y copr enable tofik/nwg-shell
 
 # Steam repo Silverblue
-#dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo
+dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo
 
 # Install extra packages Silverblue
-#dnf5 -y install bat bat-extras emacs eza fastfetch ghostty input-remapper mangohud rpm-build steam vkBasalt #  openrazer-meta
-
-# Install extra packages Bazzite
-dnf5 -y install bat bat-extras emacs eza ghostty gnome-software gnome-software-rpm-ostree htop
-dnf5 -y remove bazaar
+dnf5 -y install bat bat-extras btop emacs eza fastfetch ghostty htop input-remapper mangohud rpm-build steam vkBasalt
 
 # Window manager stuff
 dnf5 -y install cliphist hyprland hyprland-guiutils hyprpicker niri swww mako waybar waypaper blueman kde-connect kf6-kitemmodels network-manager-applet fuzzel nwg-drawer nwg-look mate-polkit mpvpaper
+
+# Bazzite packages
+#dnf5 -y install bat bat-extras emacs eza ghostty gnome-software gnome-software-rpm-ostree htop
+#dnf5 -y remove bazaar
+
 
